b14ecfea64fd5dba0e0ced6c0af71f8725aa27c7
Daniel Kioko <danielkioko.998@gmail.com>
2021-12-07 12:50:30 -0800

    code for Cv

--- Courses/ViewController.swift
+++ Courses/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
+struct Course {
+    var image: String
+    var title: String
+    var author: String
+}
+
+class CVCell: UICollectionViewCell {
+    
+    @IBOutlet var cView: UIView!
+    @IBOutlet var cImage: UIImageView!
+    @IBOutlet var cTitle: UILabel!
+    @IBOutlet var cAuthor: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        cView.layer.cornerRadius = 12
+        cImage.layer.cornerRadius = 12
+    }
+    
+}
+
 class ViewController: UIViewController {
+    
+    let cv_cellIdentifier = "CVCell"
+    
+    let cv_courses:[Course] = [
+        Course(image: "img1", title: "Gliding 101", author: "Laurent Reiffsteck"),
+        Course(image: "img1", title: "Gliding 101", author: "Laurent Reiffsteck"),
+        Course(image: "img1", title: "Gliding 101", author: "Laurent Reiffsteck"),
+        Course(image: "img1", title: "Gliding 101", author: "Laurent Reiffsteck"),
+        Course(image: "img1", title: "Gliding 101", author: "Laurent Reiffsteck"),
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+}
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cv_courses.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cv_cellIdentifier, for: indexPath) as! CVCell
+        cell.cImage.image = UIImage(named: self.cv_courses[indexPath.row].image)
+        cell.cTitle.text = self.cv_courses[indexPath.row].title
+        cell.cAuthor.text = self.cv_courses[indexPath.row].author
+        return cell
+    }
+    
 }
 
